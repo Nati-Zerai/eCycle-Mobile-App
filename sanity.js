db6d22397966b23807e30b0f28a379527c575b0a
Nati-Zerai <Natizerai11@gmail.com>
2022-11-22 13:38:10 +0400

    15th Nati - Updated sanity files

--- sanity.js
+++ sanity.js
@@ -1,13 +1,13 @@
 // import { SanityClient } from "@sanity/client";
 // import { ImageUrlBuilder } from "@sanity/image-url/lib/types/builder";
-import sanityClient  from "@sanity/client";
+import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 const client = sanityClient({
-    projectId : "ovztyqi4",
-    dataset: 'production',
-    useCdn: true,
-    apiVersion: '2021-10-21',
+  projectId: "ovztyqi4",
+  dataset: "production",
+  useCdn: true,
+  apiVersion: "2021-10-21",
 });
 
 const builder = imageUrlBuilder(client);
